a57fbc49716a28a1b5351f8cce58f967e78f0240
Muhammad Ibtesam Arshad <ibtesam@universalplus.technology>
2023-08-11 18:06:06 +0400

    added meta generator and error details in custom renderer

--- core/renderers.py
+++ core/renderers.py
@@ -1,14 +1,22 @@
 from rest_framework import renderers
 
 
+def get_meta(status='error', code=400, message='Something went wrong'):
+    return {
+        'status': status,
+        'code': code,
+        'message': message,
+    }
+
+
 class CustomJSONRenderer(renderers.JSONRenderer):
     def render(self, data=None, accepted_media_type=None, renderer_context=None):
         response = None
+        status_code = renderer_context['response'].status_code
+        exception = renderer_context['response'].exception
 
         try:
             content = data['content'] if data is not None else ''
-            status_code = renderer_context['response'].status_code
-            exception = renderer_context['response'].exception
             is_success = status_code // 100 == 2  # if status code is in 2** series
             message = data['message'] if data is not None else ''
 
@@ -16,30 +24,18 @@ class CustomJSONRenderer(renderers.JSONRenderer):
                 # Success Response
                 response = {
                     'data': content,
-                    'meta': {
-                        'status': 'success',
-                        'code': status_code,
-                        'message': message,
-                    }
+                    'meta': get_meta(status='success', code=status_code, message=message)
                 }
             else:
                 # Error Response
                 response = {
                     'error': content or str(exception),
-                    'meta': {
-                        'status': 'error',
-                        'code': status_code,
-                        'message': message,
-                    }
+                    'meta': get_meta(code=status_code, message=message)
                 }
-        except KeyError as e:
+        except KeyError:
             response = {
-                'error': str(e),
-                'meta': {
-                    'status': 'error',
-                    'code': 500,
-                    'message': 'Something went wrong',
-                }
+                'error': data,
+                'meta': get_meta(code=status_code)
             }
 
         return super().render(response, accepted_media_type, renderer_context)
